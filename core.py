4d5918ba7f291bdaa7e15d6482c46bf5a22cb58d
padhan3231 <padhan3231@gmail.com>
2026-07-12 17:29:22 +0530

    Update core.py

--- core.py
+++ core.py
@@ -25,7 +25,10 @@ def duration(filename):
                              "default=noprint_wrappers=1:nokey=1", filename],
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT)
-    return float(result.stdout)
+    try:
+        return float(result.stdout)
+    except ValueError:
+        return 0.0
     
 def exec(cmd):
         process = subprocess.run(cmd, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
@@ -153,33 +156,33 @@ def time_name():
     return f"{date} {current_time}.mp4"
 
 
-async def download_video(url,cmd, name):
+async def download_video(url, cmd, name):
     download_cmd = f'{cmd} -R 25 --fragment-retries 25 --external-downloader aria2c --downloader-args "aria2c: -x 16 -j 32"'
     global failed_counter
     print(download_cmd)
     logging.info(download_cmd)
+    
     k = subprocess.run(download_cmd, shell=True)
+    
     if "visionias" in cmd and k.returncode != 0 and failed_counter <= 10:
         failed_counter += 1
         await asyncio.sleep(5)
-        await download_video(url, cmd, name)
+        return await download_video(url, cmd, name)
+        
     failed_counter = 0
-    try:
-        if os.path.isfile(name):
-            return name
-        elif os.path.isfile(f"{name}.webm"):
-            return f"{name}.webm"
-        name = name.split(".")[0]
-        if os.path.isfile(f"{name}.mkv"):
-            return f"{name}.mkv"
-        elif os.path.isfile(f"{name}.mp4"):
-            return f"{name}.mp4"
-        elif os.path.isfile(f"{name}.mp4.webm"):
-            return f"{name}.mp4.webm"
-
+    
+    if os.path.isfile(f"{name}.mp4"):
+        return f"{name}.mp4"
+    elif os.path.isfile(f"{name}.mkv"):
+        return f"{name}.mkv"
+    elif os.path.isfile(f"{name}.webm"):
+        return f"{name}.webm"
+    elif os.path.isfile(f"{name}.mp4.webm"):
+        return f"{name}.mp4.webm"
+    elif os.path.isfile(name):
         return name
-    except FileNotFoundError as exc:
-        return os.path.isfile.splitext[0] + "." + "mp4"
+        
+    raise Exception("Yt-dlp failed to download the video. (Link might be expired, invalid, or DRM protected)")
 
 
 async def send_doc(bot: Client, m: Message,cc,ka,cc1,prog,count,name):
